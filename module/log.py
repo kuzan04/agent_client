c201c2aaca75cc8d5d22f26f5393a203748919f7
kuzan04 <kittiphum1230@hotmail.co.th>
2022-09-18 17:23:27 +0700

    Version.1

--- module/log.py
+++ module/log.py
@@ -28,19 +28,47 @@ class LogHash0:
             print(str(e))
             sys.exit(1)
 
+    def md5sum(self, filename):
+        h = hashlib.md5()
+        b = bytearray(128*1024)
+        mv = memoryview(b)
+        try:
+            with open(filename, 'rb', buffering=0) as f:
+                for n in iter(lambda : f.readinto(mv), 0):
+                    h.update(mv[:n])
+                return h.hexdigest()
+        except Exception as e:
+            print(str(e))
+            sys.exit(1)
+
+    def sha1sum(self, filename):
+        h = hashlib.sha1()
+        b = bytearray(128*1024)
+        mv = memoryview(b)
+        try:
+            with open(filename, 'rb', buffering=0) as f:
+                for n in iter(lambda : f.readinto(mv), 0):
+                    h.update(mv[:n])
+                return h.hexdigest()
+        except Exception as e:
+            print(str(e))
+            sys.exit(1)
+
     def checkLog(self, _path):
         _, _, filenames = next(walk(_path), (None, None, []))
         for l in filenames:
             fl = open(f"{_path+l}",'rb')
             le = len(fl.readlines())
             fl.close()
-            time.sleep(2)
+            time.sleep(5)
             fl1 = open(f"{_path+l}",'rb')
             nle = len(fl1.readlines())
             fl1.close()
             if nle > le:
-                h = self.sha256sum(f"{_path+l}")
-                self.message.append(f"{self.code}#{self.name}|||{socket.gethostname()}|||{platform.system()} {platform.release()}|||{_path}|||{l}|||{str(nle)}|||{h}")
+                sha256 = self.sha256sum(f"{_path+l}")
+                md5 = self.md5sum(f"{_path+l}")
+                sha1 = self.sha1sum(f"{_path+l}")
+                self.message.append(f"{self.code}#{self.name}|||{socket.gethostname()}|||{platform.system()} {platform.release()}|||{_path}|||{l}|||{str(nle)}|||{sha256}|||{md5}|||{sha1}")
             else:
                 self.message.append(None)
 
