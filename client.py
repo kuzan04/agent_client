4506cf676389e596ae0cc56e3b0b678dcd7c2b24
kuzan04 <kittiphum1230@hotmail.co.th>
2022-10-20 22:27:13 +0700

    Modify pattern process init file.

--- client.py
+++ client.py
@@ -9,7 +9,76 @@ import time
 import datetime
 import re
 import mysql.connector
+import base64
+import binascii
 from module import cert, mode, connect, sniffer
+#======================================================
+# Convert Base64 to Text
+#======================================================
+def is_base64(s):
+    try:
+        base64.b64decode(s).decode("utf-8")
+        return True
+    except binascii.Error:
+        return False
+#======================================================
+def base64ToString(b):
+    return base64.b64decode(b).decode('utf-8')
+#======================================================
+# Write config file
+#======================================================
+def write(f, h, d):
+    for i,j in zip(h, d):
+        f.write(f"{i}={j}\n")
+#======================================================
+# Checking config file
+#======================================================
+def check(_l, _c):
+    while "init.conf" not in _l:
+        try:
+            token = input("Please enter the token you have: ")
+            if is_base64(token) == True and token:
+                c_token = token
+                deatil = base64ToString(token).split("&&&")
+                if "AG1" in deatil or "AG2" in deatil or "AG3" in deatil or "AG4" in deatil:
+                    f = open(f"{_c}/init.conf", "w+")
+                    write(f, ["type", "status", "name", "host", "port", "detail", "cert"], deatil)
+                    f.close()
+                    return c_token, os.listdir(_c)
+                else:
+                    print("[Errno] Create init.conf not success, please check token incorrect.")
+            elif self.is_base64(token) == False and token:
+                print("Token incorrect.\nPlease enter again or exit process.")
+            else:
+                print("Bye.")
+                break
+        except KeyboardInterrupt:
+            print("\nBye.")
+            break
+        except Exception as e:
+            print(str(e))
+#======================================================
+# Setup config file
+#======================================================
+def setup(_c):
+    c = []
+    try:
+        f=open(os.path.join(_c, "init.conf"), "r").readlines()
+        for i in f:
+            if i.find("#") == -1:
+                x=i.split("=")
+                c.append(x[1].strip("\n"))
+            else:
+                pass
+    except Exception as e:
+        print(str(e))
+        sys.exit(1)
+    finally:
+        if len(c) < 6:
+            print("[Errno] Please check init file.")
+            sys.exit(1)
+        else:
+            return c
 #======================================================
 # Main
 #======================================================
@@ -25,23 +94,34 @@ if __name__ == "__main__":
     else:
         print("[Errno] Please check directory config missing.")
         sys.exit(1)
-    # ==================================================
-    # Connection Main Database
-    # ==================================================
-    conn = mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        password="P@ssw0rd",
-        auth_plugin="mysql_native_password",
-        database="DOL_PDPA_LOCAL"
-    )
-    while True:
-        process = mode.startTask(__location__, __config__, __ssl__, conn)._check()
-        if isinstance(process, str):
-            config, prepared = eval(process)[0], eval(process)[1]
-            if int(prepared[0]) == 0: # FTP
-                sniffer.taskSnif(config, prepared[0], 21, prepared[-1], __config__).run()
-            elif int(prepared[0]) == 1: # Syslog
-                sniffer.taskSnif(config, prepared[0], 514, prepared[-1], __config__).run()
+    # Connection Main Database.
+    conn = mysql.connector.connect( host="127.0.0.1", user="root", password="P@ssw0rd", auth_plugin="mysql_native_password", database="DOL_PDPA_LOCAL" )
+    # Call fn check.
+    __token__, c_list = check(os.listdir(__config__), __config__)
+    if c_token and c_list:
+        __init__ = setup(c_list)
+        db = None
+        if config[0] == "AG3":
+            detail = config[-1].split("&")
+            if detail[0] == 1: # MySQL.
+                db = mysql.connector.connect( host=detail[1], user=detail[2], password=detail[3], auth_plugin="mysql_native_password", database=detail[4] )
+            elif detail[0] == 0: # Hold for oracle database.
+                pass
+            else: # Hold for another service database.
+                pass
+        else:
+            db = None
+        while True:
+            process = mode.startTask(__config__, __init__, __token__, __ssl__, conn, db).run()
+            if isinstance(process, str):
+                config, prepared = eval(process)[0], eval(process)[1]
+                if int(prepared[0]) == 0: # FTP
+                    sniffer.taskSnif(config, prepared[0], 21, prepared[-1], __config__).run()
+                elif int(prepared[0]) == 1: # Syslog
+                    sniffer.taskSnif(config, prepared[0], 514, prepared[-1], __config__).run()
+                else:
+                    pass
             else:
                 pass
+    else:
+        pass
--- module/mode.py
+++ module/mode.py
@@ -1,82 +1,22 @@
 import os
 import time
-import base64
-import binascii
 import sys
 import mysql.connector
 from module import log, file, db, connect
 
 class startTask:
-    def __init__(self, default, config, ssl, conn):
-        self.config = []
-        self._content = ["type", "status", "name", "host", "port", "detail", "cert"]
-        self._path = default
-        self._config = config
-        self._list = os.listdir(config)
+    def __init__(self, path, init, token, ssl, conn, db):
+        self._config = path
         self._start = True
-        self._token = ""
+        self._token = token
         self._ssl = ssl
         self._store = {}
         self._conn = conn
-        self._select = ""
-
-    def is_base64(self, s):
-        try:
-            base64.b64decode(s).decode("utf-8")
-            return True
-        except binascii.Error:
-            return False
-
-    def base64ToString(self, b):
-        return base64.b64decode(b).decode('utf-8')
+        self._select = db
+        self.config = init
 
     def stringToBase64(self, plantText):
         return base64.b64encode(plantText)
-
-    def _listC(self, f, detail):
-        for i,j in zip(self._content, detail):
-            f.write(f"{i}={j}\n")
-
-    def _check(self):
-        while "init.conf" not in self._list:
-            try:
-                token = input("Please enter the token you have: ")
-                if self.is_base64(token) == True and token:
-                    self._token = token
-                    deatil = self.base64ToString(token).split("&&&")
-                    if "AG1" in deatil or "AG2" in deatil or "AG3" in deatil or "AG4" in deatil:
-                        f = open(f"{self._config}/init.conf", "w+")
-                        self._listC(f, deatil)
-                        f.close()
-                        self._list = os.listdir(self._config)
-                    else:
-                        print("[Errno] Create init.conf not success, please check token incorrect.")
-                elif self.is_base64(token) == False and token:
-                    print("Token incorrect.\nPlease enter again or exit process.")
-                else:
-                    print("Bye.")
-                    break
-            except KeyboardInterrupt:
-                print("\nBye.")
-                break
-            except Exception as e:
-                print(str(e))
-        return self._run()
-
-    def setupConfig(self):
-        try:
-            f=open(os.path.join(self._config, "init.conf"), "r").readlines()
-            for i in f:
-                if i.find("#") == -1:
-                    x=i.split("=")
-                    self.config.append(x[1].strip("\n"))
-        except Exception as e:
-            print(str(e))
-            sys.exit(1)
-        finally:
-            if len(self.config) < 6:
-                print("[Errno] Please check init file.")
-                sys.exit(1)
 
     def checkToken(self, fetch, i):
         if i == len(fetch):
@@ -106,7 +46,8 @@ class startTask:
                     content+=str(now)+"&&&"
             return self.stringToBase64(content.encode('utf-8')).decode('utf-8')
 
-    def _update(self, old, now, i):
+    # Hold not use today 2022/20/10 22:14
+    '''def _update(self, old, now, i):
         if i == (len(old)+len(now))/2:
             return False
         elif i == 1 and old[i] == int(now[i]):
@@ -116,19 +57,19 @@ class startTask:
         elif i > 1 or i == 0 and old[i] != now[i]:
             return True
         elif i > 1 or i == 0 and old[i] == now[i]:
-            return self._update(old, now, (i+1))
+            return self._update(old, now, (i+1))'''
 
-    def _updateFile(self, new, ip, port):
+    # Hold not use today 2022/20/10 22:07
+    '''def _updateFile(self, new, ip, port):
         new.insert(3, ip), new.insert(-1, port)
         if "AG1" in new or "AG2" in new or "AG3" in new or "AG4" in new:
             f=open(f"{self._config}/init.conf", "w+")
             self._listC(f, new)
-            f.close()
+            f.close()'''
 
     def _run(self):
-        self.setupConfig()
-        cnow = self.config
-        ip, port = cnow.pop(3), cnow.pop(-2)
+        now = self.config
+        ip, port = now.pop(3), now.pop(-2)
         while self._start:
             cursor = self._conn.cursor()
             cursor.execute('SELECT pas.code, pam.agm_status, pam.agm_name, pam.config_detail, pam.agm_token FROM TB_TR_PDPA_AGENT_MANAGE as pam JOIN TB_TR_PDPA_AGENT_STORE as pas ON pam.ags_id = pas.ags_id;')
@@ -147,49 +88,47 @@ class startTask:
             else:
                 rs = list(rs)
                 rs.pop()
-                if self._update(rs, cnow, 0) == False and rs[1] == 1:
-                    print(-1)
-                elif self._update(rs, cnow, 0) == True and rs[1] == 0:
-                    old = cnow
-                    try:
-                        self._updateFile(rs, ip, port)
-                    except KeyboardInterrupt:
-                        self._updateFile(old, ip, port)
-                    finally:
-                        self.config = []
-                        self.setupConfig()
-                elif self._update(rs, cnow, 0) == True and rs[1] == 1:
-                    self._updateFile(rs, ip, port)
-                    self.config = []
-                    self.setupConfig()
-                    if self.config[0] == "AG1":
-                        cursor.execute('SELECT path, name_file FROM TB_TR_PDPA_AGENT_LOG0_HASH;')
-                        backup = cursor.fetchall()
-                        result, store = log.LogHash0(self.config[-1].split(","), self.config[0], self.config[2], self._store, backup).run()
-                        self._store = store
-                        self._connect(result, "AG1")
-                        self._start = True
-                    elif self.config[0] == "AG2":
-                        result = file.dirFile(self.config[-1].split(","), self.config[0], self.config[2], self.config[3]).run()
-                        self._connect(result, "AG2")
-                        self._start = True
-                    elif self.config[0] == "AG3":
-                        prepared = self.config[-1].split("&")
-                        if not isinstance(self._select, mysql.connector.connection.MySQLConnection):
-                            self._select = mysql.connector.connect( host=prepared[1], user=prepared[2], password=prepared[3], database=prepared[4], auth_plugin="mysql_native_password")
-                        else:
-                            pass
-                        result = db.dbCheck(prepared[0], self.config[0], self.config[2], self._select, prepared[5:]).run()
-                        self._connect(result, "AG3")
-                        self._start = True
-                    elif self.config[0] == "AG4":
-                        prepared = self.config[-1].split(",")
-                        return f"{self.config},{prepared}"
-                        self._start = True
-                    else:
-                        print("[Errno] Type error.")
+                # Hold not use today 2022/20/2022 22:26
+                # ==================================================
+                #if self._update(rs, now, 0) == False and rs[1] == 1:
+                #    print(-1)
+                #elif self._update(rs, now, 0) == True and rs[1] == 0:
+                #     old = now
+                #     try:
+                #         self._updateFile(rs, ip, port)
+                #     except KeyboardInterrupt:
+                #         self._updateFile(old, ip, port)
+                #     finally:
+                #         self.config = []
+                #         self.setupConfig()
+                #elif self._update(rs, now, 0) == True and rs[1] == 1:
+                #    self._updateFile(rs, ip, port)
+                # ==================================================
+                if self.config[0] == "AG1":
+                    cursor.execute('SELECT path, name_file FROM TB_TR_PDPA_AGENT_LOG0_HASH;')
+                    backup = cursor.fetchall()
+                    result, store = log.LogHash0(self.config[-1].split(","), self.config[0], self.config[2], self._store, backup).run()
+                    self._store = store
+                    self._connect(result, "AG1")
+                    self._start = True
+                elif self.config[0] == "AG2":
+                    result = file.dirFile(self.config[-1].split(","), self.config[0], self.config[2], self.config[3]).run()
+                    self._connect(result, "AG2")
+                    self._start = True
+                elif self.config[0] == "AG3":
+                    result = db.dbCheck(prepared[0], self.config[0], self.config[2], self._select, prepared[5:]).run()
+                    self._connect(result, "AG3")
+                    self._start = True
+                elif self.config[0] == "AG4":
+                    prepared = self.config[-1].split(",")
+                    return f"{self.config},{prepared}"
+                    self._start = True
                 else:
-                    pass
+                    print("[Errno] Type error.")
+                # ==================================================
+                #else:
+                #    pass
+                # ==================================================
 
     def _connect(self, msg, _type):
         client_cert = [x for x in self._ssl if ".crt" in x.split("/")[-1]].pop()
