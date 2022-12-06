0bb8c6d3b0bc060a9262502ba862d375a9315338
kuzan04 <kittiphum1230@hotmail.co.th>
2022-12-07 02:13:05 +0700

    no comment.

--- module/file.py
+++ module/file.py
@@ -4,7 +4,6 @@ import os
 import time
 #import pysftp
 import ftplib
-import ssl
 from os import walk
 from os import listdir
 from os.path import isfile, join
@@ -48,8 +47,7 @@ class dirFile:
                 content_file = open(os.path.join(path, l), 'rb')
                 size = os.path.getsize(os.path.join(path, l))
                 if str(self._port)[-2:] == "21":
-                    ctx = ssl._create_stdlib_context(ssl.PROTOCOL_TLSv1_2)
-                    ftp=ftplib.FTP_TLS(context=ctx)
+                    ftp=ftplib.FTP_TLS()
                     ftp.connect(self._ip, self._port)
                     #try:
                     #    ftp.login(self.user, self.passwd)
@@ -64,7 +62,6 @@ class dirFile:
                     # 530 Login authentication failed.
                     ftp.sendcmd(f"USER {self.user}")
                     ftp.sendcmd(f"PASS {self.passwd}")
-                    ftp.prot_p()
                     # 553 Could not create file.
                     #ftp.cwd(f"/home/{self.user}/")
                     ftp.storbinary(f'STOR {name_file}', content_file)
